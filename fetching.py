d8ccb53e8cdbbfab8765caf3f248bfb9d56f1e68
Magnus S. Thrap <magnus.solheim@gmail.com>
2015-02-11 17:20:03 +0100

    Fetch all documents of a given objet_type_id in Trondheim

---
+++ fetching.py
@@ -0,0 +1,52 @@
+import json
+import math
+
+import requests
+
+
+TRONDHEIM_KOMMUNE = 1601
+VEGREFERANSE = 532
+FARTSGRENSE = 105
+
+
+def get_all(object_type_id):
+    query = {
+        "lokasjon": {
+            "kommune": [TRONDHEIM_KOMMUNE]
+        },
+        "objektTyper": [{
+            "id": object_type_id,
+            "antall": 0,
+        }]
+    }
+
+    r = requests.get("https://www.vegvesen.no/nvdb/api/sok?kriterie="+json.dumps(query))
+
+    initial_result = r.json()
+
+    number_of_objects = initial_result['resultater'][0]['statistikk']['antallFunnet']
+    number_of_objects = 10
+    OBJECTS_PER_FETCH = 10
+
+    query['objektTyper'][0]['antall'] = OBJECTS_PER_FETCH
+
+    objects = []
+    for i in range(0, int(math.ceil(float(number_of_objects)/OBJECTS_PER_FETCH))):
+        offset = OBJECTS_PER_FETCH * i
+        print offset, "/", number_of_objects, len(objects)
+
+        query['objektTyper'][0]['start'] = offset + 1  # NVDB is one indexed
+
+        r = requests.get("https://www.vegvesen.no/nvdb/api/sok?kriterie="+json.dumps(query))
+
+        result = r.json()
+
+        for veg_objekt in result['resultater'][0]['vegObjekter']:
+            objects.append(veg_objekt)
+
+    print json.dumps(objects[0], indent=2)
+    print len(objects)
+
+
+print get_all(FARTSGRENSE)
+print get_all(VEGREFERANSE)
