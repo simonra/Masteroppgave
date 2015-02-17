65c0b4c930b8e29577223a0146673a8872f5bca9
Simon Randby <simon.randby@gmail.com>
2015-02-17 15:14:40 +0100

    Possibly working floyd-warshall!

--- javakode/src/graph/FloydWarshallInterpretation.java
+++ javakode/src/graph/FloydWarshallInterpretation.java
@@ -19,6 +19,10 @@ public class FloydWarshallInterpretation {
 			allPairShortestDistances[i][i] = 0;
 		}
 
+		
+		int edgeOffset = graph.nodes.length;
+		int arcOffset = graph.nodes.length + graph.edges.length;
+		
 		/*
 		 * For each edge, set the distance between the nodes at both end as the
 		 * traversal cost of the edge, it it's shorter than the currently set
@@ -29,12 +33,16 @@ public class FloydWarshallInterpretation {
 			int node1 = currentEdge.fromNodeId - 1;
 			int node2 = currentEdge.toNodeId - 1;
 
-			if (currentEdge.passThroughCost < allPairShortestDistances[node1][node2]) {
-				allPairShortestDistances[node1][node2] = currentEdge.passThroughCost;
-			}
-			if (currentEdge.passThroughCost < allPairShortestDistances[node2][node1]) {
-				allPairShortestDistances[node2][node1] = currentEdge.passThroughCost;
-			}
+//			if (currentEdge.passThroughCost < allPairShortestDistances[node1][node2]) {
+//				allPairShortestDistances[node1][node2] = currentEdge.passThroughCost;
+//			}
+//			if (currentEdge.passThroughCost < allPairShortestDistances[node2][node1]) {
+//				allPairShortestDistances[node2][node1] = currentEdge.passThroughCost;
+//			}
+			allPairShortestDistances[edgeOffset + i][node1] = graph.nodes[node1].passThroughCost;
+			allPairShortestDistances[node1][edgeOffset + i] = graph.edges[i].passThroughCost;
+			allPairShortestDistances[edgeOffset + i][node2] = graph.nodes[node2].passThroughCost;
+			allPairShortestDistances[node2][edgeOffset + i] = graph.edges[i].passThroughCost;
 		}
 
 		// For each arc, set the distance {startnode,endnode} to the arc
@@ -44,117 +52,14 @@ public class FloydWarshallInterpretation {
 			int fromNode = currentArc.fromNodeId - 1;
 			int toNode = currentArc.toNodeId - 1;
 
-			if (currentArc.passThroughCost < allPairShortestDistances[fromNode][toNode]) {
-				allPairShortestDistances[fromNode][toNode] = currentArc.passThroughCost;
-			}
+//			if (currentArc.passThroughCost < allPairShortestDistances[fromNode][toNode]) {
+//				allPairShortestDistances[fromNode][toNode] = currentArc.passThroughCost;
+//			}
+			allPairShortestDistances[arcOffset + i][toNode] = graph.nodes[toNode].passThroughCost;
+			allPairShortestDistances[fromNode][arcOffset + i] = graph.arcs[i].passThroughCost;
 		}
 
-		/*
-		 * For each node, set the distance between each connected edge and
-		 * outbound arc as the traversal cost of the node
-		 */
-		// The -1 is to compensate for 1-indexing of edges and arc.
-		int edgeOffset = graph.nodes.length - 1;
-		int arcOffset = graph.nodes.length + graph.edges.length - 1;
-		for (int i = 0; i < graph.nodes.length; i++) {
-			Node currentNode = graph.nodes[i];
-			// for each inbound edge
-			// Set distance to each other edge and outbound arc to the nodes
-			// passthrough cost
-			// for each outbound edge
-			// Set distance to each other edge and outbound arc to the nodes
-			// passthrough cost
-			// for each inbound arc
-			// Set distance to each other edge and outbound arc to the nodes
-			// passthrough cost
-			for (int j = 0; j < currentNode.inboundEdges.length; j++) {
-				int sourceEdge = currentNode.inboundEdges[j];
-				for (int k = 0; k < currentNode.inboundEdges.length; k++) {
-					int destinationEdge = currentNode.inboundEdges[k];
-					if (sourceEdge == destinationEdge) {
-						continue;
-					}
-					if (currentNode.passThroughCost < allPairShortestDistances[sourceEdge
-							+ edgeOffset][destinationEdge + edgeOffset]) {
-						allPairShortestDistances[sourceEdge + edgeOffset][destinationEdge
-								+ edgeOffset] = currentNode.passThroughCost;
-					}
-				}
-				for (int k = 0; k < currentNode.outboundEdges.length; k++) {
-					int destinationEdge = currentNode.outboundEdges[k];
-					if (currentNode.passThroughCost < allPairShortestDistances[sourceEdge
-							+ edgeOffset][destinationEdge + edgeOffset]) {
-						allPairShortestDistances[sourceEdge + edgeOffset][destinationEdge
-								+ edgeOffset] = currentNode.passThroughCost;
-					}
-				}
-				for (int k = 0; k < currentNode.outboundArcs.length; k++) {
-					int destinationArc = currentNode.outboundArcs[k];
-					if (currentNode.passThroughCost < allPairShortestDistances[sourceEdge
-							+ edgeOffset][destinationArc + arcOffset]) {
-						allPairShortestDistances[sourceEdge + edgeOffset][destinationArc
-								+ arcOffset] = currentNode.passThroughCost;
-					}
-				}
-			}
-			for (int j = 0; j < currentNode.outboundEdges.length; j++) {
-				int sourceEdge = currentNode.outboundEdges[j];
-				for (int k = 0; k < currentNode.inboundEdges.length; k++) {
-					int destinationEdge = currentNode.inboundEdges[k];
-					if (currentNode.passThroughCost < allPairShortestDistances[sourceEdge
-							+ edgeOffset][destinationEdge + edgeOffset]) {
-						allPairShortestDistances[sourceEdge + edgeOffset][destinationEdge
-								+ edgeOffset] = currentNode.passThroughCost;
-					}
-				}
-				for (int k = 0; k < currentNode.outboundEdges.length; k++) {
-					int destinationEdge = currentNode.outboundEdges[k];
-					if (sourceEdge == destinationEdge) {
-						continue;
-					}
-					if (currentNode.passThroughCost < allPairShortestDistances[sourceEdge
-							+ edgeOffset][destinationEdge + edgeOffset]) {
-						allPairShortestDistances[sourceEdge + edgeOffset][destinationEdge
-								+ edgeOffset] = currentNode.passThroughCost;
-					}
-				}
-				for (int k = 0; k < currentNode.outboundArcs.length; k++) {
-					int destinationArc = currentNode.outboundArcs[k];
-					if (currentNode.passThroughCost < allPairShortestDistances[sourceEdge
-							+ edgeOffset][destinationArc + arcOffset]) {
-						allPairShortestDistances[sourceEdge + edgeOffset][destinationArc
-								+ arcOffset] = currentNode.passThroughCost;
-					}
-				}
-			}
-			for (int j = 0; j < currentNode.inboundArcs.length; j++) {
-				int sourceArc = currentNode.inboundArcs[j];
-				for (int k = 0; k < currentNode.inboundEdges.length; k++) {
-					int destinationEdge = currentNode.inboundEdges[k];
-					if (currentNode.passThroughCost < allPairShortestDistances[sourceArc
-							+ arcOffset][destinationEdge + edgeOffset]) {
-						allPairShortestDistances[sourceArc + arcOffset][destinationEdge
-								+ edgeOffset] = currentNode.passThroughCost;
-					}
-				}
-				for (int k = 0; k < currentNode.outboundEdges.length; k++) {
-					int destinationEdge = currentNode.outboundEdges[k];
-					if (currentNode.passThroughCost < allPairShortestDistances[sourceArc
-							+ arcOffset][destinationEdge + edgeOffset]) {
-						allPairShortestDistances[sourceArc + arcOffset][destinationEdge
-								+ edgeOffset] = currentNode.passThroughCost;
-					}
-				}
-				for (int k = 0; k < currentNode.outboundArcs.length; k++) {
-					int destinationArc = currentNode.outboundArcs[k];
-					if (currentNode.passThroughCost < allPairShortestDistances[sourceArc
-							+ arcOffset][destinationArc + edgeOffset]) {
-						allPairShortestDistances[sourceArc + arcOffset][destinationArc
-								+ edgeOffset] = currentNode.passThroughCost;
-					}
-				}
-			}
-		}
+
 
 		String outputAllPairsString = "";
 		for (int i = 0; i < allPairShortestDistances.length; i++) {
@@ -179,6 +84,25 @@ public class FloydWarshallInterpretation {
 				}
 			}
 
+		}
+		
+		//Subtract destination cost from destinations
+		double amountToSubtract;
+		for (int i = 0; i < numberOfElementsInGraph; i++) {
+			if(i < edgeOffset){
+				amountToSubtract = graph.nodes[i].passThroughCost;
+			}
+			else if(i < arcOffset){
+				amountToSubtract = graph.edges[i - edgeOffset].passThroughCost;
+			}else {
+				amountToSubtract = graph.arcs[i - arcOffset].passThroughCost;
+			}
+			for (int j = 0; j < numberOfElementsInGraph; j++) {
+				if(i == j){
+					continue;
+				}
+				allPairShortestDistances[j][i] -= amountToSubtract;
+			}
 		}
 
 		return allPairShortestDistances;
