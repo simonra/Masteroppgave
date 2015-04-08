ced411f30aaa482e4eaeecf27173678799a0c4ac
Simon Randby <simon.randby@gmail.com>
2015-04-08 17:56:46 +0200

    Some testing run,lots of uneccessary stuff removed

--- javakode/src/parameterFiles/GraphParams.java
+++ javakode/src/parameterFiles/GraphParams.java
@@ -3,8 +3,8 @@ package parameterFiles;
 public class GraphParams {
 
 //	public static final String GRAPH_FILE_PATH = "resources/DI-NEARP-n833-Q16k.dat";
-	public static final String GRAPH_FILE_PATH = "resources/femnodegraf.dat";
+//	public static final String GRAPH_FILE_PATH = "resources/femnodegraf.dat";
 //	public static final String GRAPH_FILE_PATH = "resources/mggdb_myTest.dat";
-//	public static final String GRAPH_FILE_PATH = "resources/mggdb_0.25_1.dat";
+	public static final String GRAPH_FILE_PATH = "resources/mggdb_0.25_1.dat";
 //	public static final String GRAPH_FILE_PATH = "resources/mgval_0.50_10D.dat";
 }
