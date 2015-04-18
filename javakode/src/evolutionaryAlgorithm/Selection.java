d05b0818975ce3280f02c284b91e5b1a513dd881
Simon Randby <simon.randby@gmail.com>
2015-04-18 17:43:50 +0200

    Added parent selection with replacement
    This allows for (amongst other things) overproduction, as well as
    a "truer" representation of the world (to the extent that that is
    relephant) by allowing inidviduals to have several mates and produce
    a larger proportion of each generations offspring if rng-jesus so
    desires.

--- javakode/src/evolutionaryAlgorithm/Selection.java
+++ javakode/src/evolutionaryAlgorithm/Selection.java
@@ -16,11 +16,15 @@ public class Selection {
 	/**Depending on how the run is configured moves a combination
 	 * of old adults and new children to the next generation and clears children*/
 	public static void adultSelection(ArrayList<Genotype> adultPopulation, ArrayList<Genotype> children){
+		for (Genotype child : children) {
+			if(adultPopulation.contains(child)){
+				System.out.println("There already is one like that");
+			}
+		}
 
 		if(EvolutionaryAlgorithmParams.ADULT_SELECTION == AdultSelection.FULL_REPLACEMENT){
 			adultPopulation.clear();
 			adultPopulation.addAll(children);
-			children.clear();
 		}
 
 		else if(EvolutionaryAlgorithmParams.ADULT_SELECTION == AdultSelection.ELITIST_MIXING){
@@ -28,49 +32,77 @@ public class Selection {
 			adultPopulation.clear();
 			Collections.sort(children);
 			adultPopulation.addAll(children.subList(children.size() - EvolutionaryAlgorithmParams.POPULATION_SIZE, children.size()));
-			children.clear();
+		}
+		
+		else if(EvolutionaryAlgorithmParams.ADULT_SELECTION == AdultSelection.RANDOM_MIXING){
+			children.addAll(adultPopulation);
+			adultPopulation.clear();
+			while(adultPopulation.size() < EvolutionaryAlgorithmParams.POPULATION_SIZE){
+				adultPopulation.add(children.remove(Utilities.getRandom().nextInt(children.size())));
+			}
 		}
 
 		else if(EvolutionaryAlgorithmParams.ADULT_SELECTION == AdultSelection.OVERPRODUCTION){
 			adultPopulation.clear();
-			Collections.sort(children);
-			adultPopulation.addAll(children.subList(children.size() - EvolutionaryAlgorithmParams.POPULATION_SIZE, children.size()));
-			children.clear();
+			
+			while(adultPopulation.size() < EvolutionaryAlgorithmParams.POPULATION_SIZE){
+				adultPopulation.add(children.remove(Utilities.getRandom().nextInt(children.size())));
+			}
+			
+//			Collections.sort(children);
+//			adultPopulation.addAll(children.subList(children.size() - EvolutionaryAlgorithmParams.POPULATION_SIZE, children.size()));
 		}
-
+		
+		children.clear();
 	}
 
 	public static ArrayList<Genotype> selectParents(ArrayList<Genotype> adultPopulation){
 		ArrayList<Genotype> population = new ArrayList<>();
 		population.addAll(adultPopulation);
 		ArrayList<Genotype> selectedParents = new ArrayList<>();
+		Genotype lastSelected = null;
+		Genotype toBeAdded;
 
 
 		if(EvolutionaryAlgorithmParams.PARENT_SELECTION == ParentSelection.UNIFORM_SELECTION){
 			while(selectedParents.size() < EvolutionaryAlgorithmParams.NUMBER_OF_CROSSOVER_PAIRS * 2){
-				selectedParents.add(population.remove(Utilities.getRandom().nextInt(population.size())));
+				toBeAdded = population.remove(Utilities.getRandom().nextInt(population.size()));
+				if(!toBeAdded.equals(lastSelected)){
+					selectedParents.add(toBeAdded);
+					lastSelected = toBeAdded;
+				}
+				population.add(toBeAdded);
 			}
 		}
 		
 		else if(EvolutionaryAlgorithmParams.PARENT_SELECTION == ParentSelection.TOURNAMENT_SELECTION){
+			ArrayList<Genotype> tournamentGroup = new ArrayList<>();
+			ArrayList<Genotype> tournamentGroupCopy = new ArrayList<>();
 			while(selectedParents.size() < parameterFiles.EvolutionaryAlgorithmParams.NUMBER_OF_CROSSOVER_PAIRS * 2){
-				ArrayList<Genotype> tournamentGroup = new ArrayList<>();
-				ArrayList<Genotype> tournamentGroupCopy = new ArrayList<>();
+				tournamentGroup.clear();
+				tournamentGroupCopy.clear();
 				while(tournamentGroup.size() < EvolutionaryAlgorithmParams.TOURNAMENT_SIZE){
-					tournamentGroup.add(population.remove(Utilities.getRandom().nextInt(population.size())));
+					toBeAdded = population.remove(Utilities.getRandom().nextInt(population.size()));
+					if(!toBeAdded.equals(lastSelected)){
+						tournamentGroup.add(toBeAdded);
+					}else{
+						population.add(toBeAdded);
+					}
 				}
 				Collections.sort(tournamentGroup);
 				tournamentGroupCopy.addAll(tournamentGroup);
 				for (int i = 0; i < EvolutionaryAlgorithmParams.TOURNAMENT_SIZE - 1; i++) {
 					if(Utilities.getRandom().nextDouble() < EvolutionaryAlgorithmParams.TOURNAMEN_SELECTION_PROBABILITY){
-						selectedParents.add(tournamentGroup.remove(tournamentGroup.size() - 1));
+						lastSelected = tournamentGroup.remove(tournamentGroup.size() - 1);
+						selectedParents.add(lastSelected);
 //						tournamentGroupCopy.remove(tournamentGroup.size());	//is the same as the index of the element just removed
 						break;
 					}
 					tournamentGroup.remove(tournamentGroup.size() - 1);
 					/*If everybody else failed the tournament, the worst participant is selected with a probability of 1:*/
 					if(tournamentGroup.size() == 1){
-						selectedParents.add(tournamentGroup.remove(0));
+						lastSelected = tournamentGroup.remove(0);
+						selectedParents.add(lastSelected);
 //					tournamentGroupCopy.remove(0);
 					}
 				}
@@ -89,21 +121,26 @@ public class Selection {
 			for (Genotype genotype : population) {
 				sumOfFitnesses += genotype.getFitness();
 			}
+			double cumulativeDensityOfPrevious = 0;
+			for (Genotype genotype : population) {
+//				genotype.calculateFitness();	//For when we have removed one of its brethren in a previous iteration
+				genotype.setFitness((genotype.getFitness()/sumOfFitnesses) + cumulativeDensityOfPrevious);
+				cumulativeDensityOfPrevious += genotype.getFitness();
+			}
+			Collections.sort(population);
 			while(selectedParents.size() < EvolutionaryAlgorithmParams.NUMBER_OF_CROSSOVER_PAIRS * 2){
-				double cumulativeDensityOfPrevious = 0;
-				for (Genotype genotype : population) {
-					genotype.calculateFitness();	//For when we have removed one of its brethren in a previous iteration
-					genotype.setFitness((genotype.getFitness()/sumOfFitnesses) + cumulativeDensityOfPrevious);
-					cumulativeDensityOfPrevious += genotype.getFitness();
-				}
-				Collections.sort(population);
 				double random = Utilities.getRandom().nextDouble();
 				for (int i = 0; i < population.size(); i++) {
 					if(random < population.get(i).getFitness()){
-						population.get(i).calculateFitness();
-						sumOfFitnesses -= population.get(i).getFitness();
-						selectedParents.add(population.remove(i));
-						break;
+//						population.get(i).calculateFitness();
+//						sumOfFitnesses -= population.get(i).getFitness();
+						toBeAdded = population.get(i);
+						if(!toBeAdded.equals(lastSelected)){
+							selectedParents.add(toBeAdded);
+							lastSelected = toBeAdded;
+							break;
+						}
+//						selectedParents.add(population.remove(i));
 					}
 				}
 				
