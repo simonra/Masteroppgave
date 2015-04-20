ddb293ab9be95f89d901ca1092f11bea5beaf509
Simon Randby <simon.randby@gmail.com>
2015-04-20 17:01:02 +0200

    Misch working in progress stuff
    Most importantly add easy access to the new bhw cases in the graph-
    parameters file.

--- javakode/src/parameterFiles/EvolutionaryAlgorithmParams.java
+++ javakode/src/parameterFiles/EvolutionaryAlgorithmParams.java
@@ -8,26 +8,26 @@ public class EvolutionaryAlgorithmParams {
 		GRAND_TOUR, SPLITTED;
 	}
 	public enum AdultSelection{
-		FULL_REPLACEMENT, ELITIST_MIXING, OVERPRODUCTION;
+		FULL_REPLACEMENT, ELITIST_MIXING, RANDOM_MIXING, OVERPRODUCTION;
 	}
 	
 	/**Set to Long.MAX_VALUE for in practice infinite runtime*/
 //	public static final long MAX_GENERATIONS = Long.MAX_VALUE;
 	public static final long MAX_GENERATIONS = 100000;
-//	public static final long MAX_GENERATIONS_WITHOUT_CHANGE = 50000;
-	public static final long MAX_GENERATIONS_WITHOUT_CHANGE = Long.MAX_VALUE;
+	public static final long MAX_GENERATIONS_WITHOUT_CHANGE = 50000;
+//	public static final long MAX_GENERATIONS_WITHOUT_CHANGE = Long.MAX_VALUE;
 	
-	public static final int POPULATION_SIZE = 50;
-	public static final int NUMBER_OF_CROSSOVER_PAIRS = 25;
-	public static fitnessType FINTESS_TYPE = fitnessType.GRAND_TOUR;
+	public static final int POPULATION_SIZE = 100;
+	public static final int NUMBER_OF_CROSSOVER_PAIRS = 50;
+	public static fitnessType FINTESS_TYPE = fitnessType.SPLITTED;
 	//mut-chance: always there?
-	public static final boolean PENALIZE_DEMAND_OUT_OF_ORDER = true;
+	public static final boolean PENALIZE_DEMAND_OUT_OF_ORDER = false;
 	/**Whether mutation should be random, or we should use the memetic approach*/
-	public static final boolean RANDOM_MUTATION = true;
+	public static final boolean RANDOM_MUTATION = false;
 	/**What kind of parent selection should be performed when selecting who gets to mate*/
-	public static ParentSelection PARENT_SELECTION = ParentSelection.UNIFORM_SELECTION;
+	public static ParentSelection PARENT_SELECTION = ParentSelection.FITNESS_PROPORTIONATE_SELECTION;
 	/**What kind of adult selection should be performed when one decides what individuals should survive till the next generation.*/
-	public static AdultSelection ADULT_SELECTION = AdultSelection.ELITIST_MIXING;
+	public static AdultSelection ADULT_SELECTION = AdultSelection.FULL_REPLACEMENT;
 	/**Number of genomes in a tournament if the parent selection is done tournament style*/
 	public static final int TOURNAMENT_SIZE = 5;
 	/**Likelyhood of best best individual being chosen in a tournament selection*/
