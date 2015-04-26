a7f4c82c14e65a7eb477422ae6a0a2ee69140f7c
Simon Randby <simon.randby@gmail.com>
2015-04-26 18:21:41 +0200

    Added control of population size runs

--- javakode/src/evolutionaryAlgorithm/EvolutionaryAlgorithm.java
+++ javakode/src/evolutionaryAlgorithm/EvolutionaryAlgorithm.java
@@ -131,8 +131,9 @@ public class EvolutionaryAlgorithm {
 		output += "Generation:\t" + generationNumber + "\t" + 
 				"Best fitness:\t" + bestIndividual.fitness  + "\t" +
 				"Average fitness:\t" + averageFitness + "\t" +
-				"Fitness standard deviation:\t" + fitnessStandardDev + "\t" +
-				"Best genome:\t" + bestIndividual.getFormatedGenotypeTabSeparated();
+				"Fitness standard deviation:\t" + fitnessStandardDev;
+//				+ "\t" +
+//				"Best genome:\t" + bestIndividual.getFormatedGenotypeTabSeparated();
 		
 		return output;
 	}
